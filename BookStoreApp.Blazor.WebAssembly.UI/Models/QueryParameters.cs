a73318618e4cdb3c52ebb819b3b3269275bf76bb
kev <kev@daryus.com>
2025-10-12 16:19:40 +0100

    cip...72r amended QueryParameters.cs as tw's. previously, i accepted the copilot autocomplete and left it as-is because i expected it to be "better". in hindsight, i realise that this virtualisation concept is simpler than i, at first, thought.

--- BookStoreApp.Blazor.WebAssembly.UI/Models/QueryParameters.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Models/QueryParameters.cs
@@ -1,14 +1,12 @@
 namespace BookStoreApp.Blazor.WebAssembly.UI.Models;
 
-public class QueryParameters //cip...65 (+ chatgpt)
+public class QueryParameters //cip...65
 {
-    private const int maxPageSize = 50;
-    private int _pageSize = 10;
-    public int StartIndex { get; set; } = 0;
+    private int _pageSize = 15;
+    public int StartIndex { get; set; }
     public int PageSize
     {
         get { return _pageSize; }
-        set { _pageSize = (value > maxPageSize) ? maxPageSize : value; }
+        set { _pageSize = value; }
     }
-
 }
